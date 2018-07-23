33eed2ba25311e61184a36b6c5000baab1ba2513
Webklex <info@webklex.com>
2018-07-23 23:17:23 +0200

    Config loading fixed and moved to a custom solution

--- src/IMAP/Providers/LaravelServiceProvider.php
+++ src/IMAP/Providers/LaravelServiceProvider.php
@@ -48,7 +48,46 @@ class LaravelServiceProvider extends ServiceProvider {
             return $app[ClientManager::class]->account();
         });
 
-        $this->mergeConfigFrom(__DIR__.'/../../config/imap.php', 'imap');
+        $this->setVendorConfig();
+    }
 
+    /**
+     * Merge the vendor settings with the local config
+     *
+     * The default account identifier will be used as default for any missing account parameters.
+     * If however the default account is missing a parameter the package default account parameter will be used.
+     * This can be disabled by setting imap.default in your config file to 'false'
+     */
+    private function setVendorConfig(){
+
+        $config_key = 'imap';
+        $path = __DIR__.'/../../config/'.$config_key.'.php';
+
+        $vendor_config = require $path;
+        $config = $this->app['config']->get($config_key, []);
+
+        $this->app['config']->set($config_key, array_merge($vendor_config, $config));
+
+        $config = $this->app['config']->get($config_key);
+
+        if(is_array($config)){
+            if(isset($config['default'])){
+                if(isset($config['accounts']) && $config['default'] != false){
+
+                    $default_config = $vendor_config['accounts']['default'];
+                    if(isset($config['accounts'][$config['default']])){
+                        $default_config = array_merge($default_config, $config['accounts'][$config['default']]);
+                    }
+
+                    if(is_array($config['accounts'])){
+                        foreach($config['accounts'] as $account_key => $account){
+                            $config['accounts'][$account_key] = array_merge($default_config, $account);
+                        }
+                    }
+                }
+            }
+        }
+
+        $this->app['config']->set($config_key, $config);
     }
 }
