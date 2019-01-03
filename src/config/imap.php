945668f4aa56df4d25c3cc2c87bce4a4c09e1d6c
mmjjb <jobvdbroek@gmail.com>
2019-01-03 21:33:00 +0100

    Feature - Defined check FT_UID
    Add defined check for constant FT_UID

--- src/config/imap.php
+++ src/config/imap.php
@@ -102,7 +102,7 @@ return [
     */
     'options' => [
         'delimiter' => '/',
-        'fetch' => FT_UID,
+        'fetch' => (defined('FT_UID') ? FT_UID : 0),
         'fetch_body' => true,
         'fetch_attachment' => true,
         'fetch_flags' => true,
