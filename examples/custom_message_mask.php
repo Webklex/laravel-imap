0baa7cbb0e71f9a27fb3e03358d26fe7607e9811
Webklex <info@webklex.com>
2020-08-24 18:36:22 +0200

    Custom mask method renamed to prevent confusion

--- examples/custom_message_mask.php
+++ examples/custom_message_mask.php
@@ -16,7 +16,7 @@ class CustomMessageMask extends \Webklex\IMAP\Support\Masks\MessageMask {
      * New custom method which can be called through a mask
      * @return string
      */
-    public function token(){
+    public function my_token(){
         return implode('-', [$this->message_id, $this->uid, $this->message_no]);
     }
 
@@ -43,7 +43,7 @@ $message = $folder->query()->limit(1)->get()->first();
 /** @var CustomMessageMask $masked_message */
 $masked_message = $message->mask(CustomMessageMask::class);
 
-echo 'Token for uid ['.$masked_message->uid.']: '.$masked_message->token().' @atms:'.$masked_message->getAttachmentCount();
+echo 'Token for uid ['.$masked_message->uid.']: '.$masked_message->my_token().' @atms:'.$masked_message->getAttachmentCount();
 
 $masked_message->setFlag('seen');
 
